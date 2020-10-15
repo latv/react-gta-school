f1e9589a4b2dc99911975cc24522222055b73646
Janis Feldmanis <feldmanis45@gmail.com>
2020-10-15 19:17:51 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/gta_logo.png';
+import lv from './images/lv.png';
+import ru from './images/ru.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+          <div className="wrapper">
+              <header>
+                  
+                  <ul className="nav-bar">
+
+
+
+                      <li><a href="index.html"><img className="logo" src={logo} alt=""/></a></li>
+
+
+
+                      <div className="dropdown">
+                          <button className="dropbtn">GTA versijas 
+                            <i className="fa fa-caret-down"></i>
+                          </button>
+                      
+                                  <div className="dropdown-content">
+                                  
+                                      <li><a href="GTA.html">GTA</a></li>
+                                      <li><a href="GTA2.html">GTA 2</a></li>
+                                      <li><a href="GTA3.html">GTA III</a></li>
+                                      <li><a href="GTA_Vice_City.html">GTA: Vice City</a></li>
+                                      <li><a href="GTASanAndreas.html">GTA: San Andreas</a></li>
+                                      <li><a href="GTA4.html">GTA IV</a></li>
+                                      <li><a href="GTA5.html">GTA V</a></li>
+                                      
+                                  </div>
+                        </div> 
+                        <li><a href="gallery.html">Gallery</a></li>
+                        <li><a href="about.html">Par</a></li>
+                      <div className="languge">
+                          <li><a href="#" ><img src={lv} alt=""/></a></li>
+                          <li><a href="#" ><img src={ru} alt=""/></a></li>
+                      </div>
+                  </ul>
+              </header>
+
+              
+
+
+             
+
+          </div>
+
+
   );
 }
 
